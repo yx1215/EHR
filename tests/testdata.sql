1eadafc3c495034550cd784a8baa7a83b68f6f86
Ariana Cai <xc984@nyu.edu>
2019-10-07 20:59:23 +0800

    testdata

--- tests/testdata.sql
+++ tests/testdata.sql
@@ -1,21 +1,21 @@
 
-INSERT INTO patient (first_name, last_name, password, repeat_password, email, phone_number, gender, height, weight, data_of_birth, emergency_contacts)
+INSERT INTO patients (first_name, last_name, password, repeat_password, email, phone_number, gender, height, weight, data_of_birth, emergency_contacts)
 VALUES
   ('alice', 'A', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f','pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f', 
   'alice@nyu.edu','12345678910','female','160.0','45.5','1998-11-18','12345678910'),
   ('bob', 'B', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79',
   'bob@nyu.edu','12345678910','male','170.0','55.0','1998-03-03','12345678910');
 
-INSERT INTO doctor (first_name, last_name, password, repeat_password, email, phone_number, gender)
+INSERT INTO doctors (first_name, last_name, password, repeat_password, email, phone_number, gender)
 VALUES
   ('alice', 'A', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f','pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f', 
-  'alice@nyu.edu','12345678910','female','160.0','45.5','1998-11-18','12345678910'),
+  'alice@nyu.edu','12345678910','female'),
   ('bob', 'B', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79',
-  'bob@nyu.edu','12345678910','male','170.0','55.0','1998-03-03','12345678910');
+  'male','12345678910');
 
-INSERT INTO admin (first_name, last_name, password, repeat_password, email, phone_number, gender, secret_key)
+INSERT INTO administrators (first_name, last_name, password, repeat_password, email, phone_number, gender, secret_key)
 VALUES
   ('alice', 'A', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f','pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f', 
-  'alice@nyu.edu','12345678910','female','160.0','45.5','1998-11-18','12345678910'),
+  'female','12345'),
   ('bob', 'B', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79',
-  'bob@nyu.edu','12345678910','male','170.0','55.0','1998-03-03','12345678910');
+  'male','12345');
