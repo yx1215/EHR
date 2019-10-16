6b4b2ffbda26d1565b7a66028a00700110ee4097
yx1215 <yx1215@nyu.edu>
2019-10-16 22:45:26 +0800

    Merge branch 'master' of https://github.com/yx1215/Software_engineering
    # Conflicts:
    #	.coverage
    #	tests/testdata.sql

--- tests/testdata.sql
+++ tests/testdata.sql
@@ -11,11 +11,11 @@ VALUES
   ('alice', 'A', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f',
   'alice@nyu.edu','12345678910','female'),
   ('bob', 'B', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79',
-  'male','12345678910');
+  'male','12345678910', 'female');
 
-INSERT INTO administrators (first_name, last_name, password, email, phone_number, gender, secret_key)
+INSERT INTO administrators (first_name, last_name, password, email, phone_number, gender)
 VALUES
   ('alice', 'A', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f',
-  'female','12345'),
+  'alice@.nyu.edu', 'female','12345'),
   ('bob', 'B', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79',
-  'male','12345');
+  'bob@nyu.edu', 'male','12345');
