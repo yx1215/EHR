6e3ede840c5e2d952c56c8591306d6edd64b103d
yx1215 <yx1215@nyu.edu>
2019-11-18 14:05:45 +0800

    change

--- main_app/doctor.py
+++ main_app/doctor.py
@@ -1,8 +1,6 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
-from werkzeug.exceptions import abort
-from werkzeug.security import generate_password_hash
 
 from main_app.authentication import login_required
 from main_app.database import get_db
@@ -10,17 +8,22 @@ from main_app.database import get_db
 doctor_bp = Blueprint('doctor', __name__, url_prefix="/doctor")
 
 
-@doctor_bp.route("/doctor_page/<int:id>", methods=('GET', 'POST'))
+@doctor_bp.route("/doctor_page", methods=('GET', 'POST'))
 @login_required
-def show_main(id):
+def show_main():
     db = get_db()
     pending_appointment = db.execute("SELECT * FROM appointment JOIN patients on patients.id=appointment.patient_id "
                                      "WHERE doctor_id=? and status=?",
-                                     (id, 'pending')).fetchall()
-    accepted_appointment = db.execute("SELECT * FROM appointment JOIN patients on patients.id=appointment.patient_id "
-                                      "WHERE doctor_id=? and status=?",
-                                     (id, 'accepted')).fetchall()
-    return render_template('/doctor.html', doctor_id=id, pending=pending_appointment, accept=accepted_appointment)
+                                     (g.user["id"], 'pending')).fetchall()
+    my_patient = db.execute("SELECT * FROM take_care JOIN patients on patients.id=take_care.patient_id "
+                                      "WHERE doctor_id=?",
+                                     (g.user["id"], )).fetchall()
+    my_schedule = db.execute("SELECT schedule.start_time, schedule.duration, a.location, p.last_name, p.first_name FROM schedule "
+                             "LEFT OUTER JOIN appointment a on schedule.doctor_id = a.doctor_id and schedule.start_time=a.start_time "
+                             "LEFT OUTER JOIN patients p on a.patient_id = p.id WHERE schedule.doctor_id=?",
+                             (g.user["id"], )).fetchall()
+
+    return render_template('/doctor.html', pending=pending_appointment, my_patient=my_patient, my_schedule=my_schedule)
 
 
 @doctor_bp.route("/accept_appointment", methods=('POST', ))
@@ -32,10 +35,11 @@ def accept_appointment():
     db = get_db()
     db.execute("UPDATE schedule SET occupied=true WHERE doctor_id=? AND start_time=?", (doctor_id, start_time))
     db.execute("UPDATE appointment SET status = 'accepted' WHERE patient_id=? and doctor_id=? and start_time=?", (patient_id, doctor_id, start_time))
-    db.execute("INSERT INTO take_care (doctor_id, patient_id) VALUES (?, ?)", (doctor_id, patient_id))
+    if db.execute("SELECT * FROM take_care WHERE doctor_id=? AND patient_id=?", (doctor_id, patient_id)).fetchone() is None:
+        db.execute("INSERT INTO take_care (doctor_id, patient_id) VALUES (?, ?)", (doctor_id, patient_id))
     db.commit()
 
-    return redirect(url_for('doctor.show_main', id=doctor_id))
+    return redirect(url_for('doctor.show_main'))
 
 
 @doctor_bp.route("/set_schedule", methods=('POST', ))
@@ -49,5 +53,42 @@ def set_schedule():
     db.execute("INSERT INTO schedule (doctor_id, start_time, duration, occupied) VALUES (?, ?, ?, ?)", (doctor_id, start_time, duration, False))
     db.commit()
 
-    return redirect(url_for('doctor.show_main', id=doctor_id))
+    return redirect(url_for('doctor.show_main'))
 
+
+@doctor_bp.route("/delete_schedule", methods=('POST', ))
+@login_required
+def delete_schedule():
+    doctor_id = request.args["doctor_id"]
+    start_time = request.args["start_time"]
+    print(doctor_id, start_time)
+    db = get_db()
+    db.execute("DELETE FROM schedule WHERE start_time=? AND doctor_id=?", (start_time, doctor_id))
+    db.commit()
+
+    return redirect(url_for('doctor.show_main'))
+
+
+@doctor_bp.route("/check_out", methods=('POST', ))
+@login_required
+def check_out():
+    doctor_id = request.args["doctor_id"]
+    patient_id = request.args["patient_id"]
+    print(doctor_id, patient_id)
+    # db = get_db()
+    # db.execute("DELETE FROM take_care WHERE doctor_id=? AND patient_id=?", (doctor_id, patient_id))
+    # db.commit()
+
+    return redirect(url_for('doctor.show_main'))
+
+
+@doctor_bp.route("/write_medical_his", methods=('POST', ))
+@login_required
+def write_medical_his():
+    patient_id = request.args["patient_id"]
+    medical_his = request.args["medical_his"]
+    print(patient_id, medical_his)
+    db = get_db()
+    db.execute("UPDATE patients SET medical_his=? WHERE id=?", (medical_his, patient_id))
+    db.commit()
+    return redirect(url_for("doctor.show_main"))
--- main_app/patient.py
+++ main_app/patient.py
@@ -10,13 +10,17 @@ from main_app.database import get_db
 patient_bp = Blueprint('patient', __name__, url_prefix="/patient")
 
 
-@patient_bp.route("/patient_page/<int:id>", methods=('GET', 'POST'))
+@patient_bp.route("/patient_page", methods=('GET', 'POST'))
 @login_required
-def show_main(id):
+def show_main():
     db = get_db()
-    provider = db.execute("SELECT * FROM take_care JOIN doctors ON take_care.doctor_id = doctors.id WHERE patient_id=?", (id, )).fetchone()
+    provider = db.execute("SELECT * FROM take_care JOIN doctors ON take_care.doctor_id = doctors.id WHERE patient_id=?", (g.user['id'], )).fetchone()
     doctors = db.execute("SELECT * FROM doctors JOIN schedule s on doctors.id = s.doctor_id ORDER BY s.doctor_id").fetchall()
-    return render_template('/patient.html', patient_id=id, my_provider=provider, all_doctors=doctors)
+    if provider is not None:
+        my_provider_schedule = db.execute("SELECT * FROM schedule WHERE doctor_id=?", (provider["id"], ))
+    else:
+        my_provider_schedule = None
+    return render_template('/patient.html', my_provider=provider, all_doctors=doctors, my_provider_schedule=my_provider_schedule)
 
 
 @patient_bp.route("/send_appointment", methods=('POST', ))
@@ -31,4 +35,22 @@ def send_appointment():
     db.execute('INSERT INTO appointment (doctor_id, patient_id, start_time, duration, location, status) '
                'VALUES (?, ?, ?, ?, ?, ?)', (doctor_id, patient_id, start_time, duration, 'not decided', 'pending'))
     db.commit()
-    return redirect(url_for('patient.show_main', id=patient_id))
+    return redirect(url_for('patient.show_main'))
+
+
+@patient_bp.route("/make_appointment_with_current_provider", methods=('POST', ))
+@login_required
+def make_appointment_with_current_provider():
+    doctor_id = request.args.get("doctor_id")
+    patient_id = g.user['id']
+    schedule_duration = request.args.get("schedule_duration")
+    lst = schedule_duration.strip().split(",")
+    start_time = lst[0].strip()
+    duration = lst[1][:-3].strip()
+    print(doctor_id, start_time, duration)
+    db = get_db()
+    db.execute('INSERT INTO appointment (doctor_id, patient_id, start_time, duration, location, status) '
+               'VALUES (?, ?, ?, ?, ?, ?)', (doctor_id, patient_id, start_time, duration, 'not decided', 'pending'))
+    db.commit()
+
+    return redirect(url_for('patient.show_main'))
